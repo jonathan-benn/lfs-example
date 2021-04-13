719af98379f12fcbabd621ccd7de43f764d9455b
Jonathan Benn <jonathan.benn@sap.com>
2021-04-13 16:46:34 -0400

    Added new test that should pass

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
+"use strict"
+
 const add = require('../code/add.js');
 
 QUnit.module('add')
 
 QUnit.test('add two numbers', assert => {
   assert.equal(add(1, 1), 2);
+});
+
+QUnit.test('this should pass', assert => {
+  assert.equal(add(2, 2), 4);
 });
